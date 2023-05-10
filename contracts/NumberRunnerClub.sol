af48343faab0b1de42e40fee3b548333ccc4c2cd
dassman <lukas-8@live.fr>
2023-05-10 15:39:45 +0200

    add mapping balance for every type of NFT

--- contracts/NumberRunnerClub.sol
+++ contracts/NumberRunnerClub.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NumberRunnerClub is ERC721 {
+contract NumberRunnerClub is ERC721URIStorage {
     using Counters for Counters.Counter;
     enum Piece {
         King,
@@ -23,24 +23,31 @@ contract NumberRunnerClub is ERC721 {
     uint256 public constant MAX_BISHOP_SUPPLY = 200;
 
     Counters.Counter private _tokenIds;
-    Counters.Counter private _tokenIds;
     Counters.Counter private _kingCounter;
     Counters.Counter private _queenCounter;
     Counters.Counter private _rookCounter;
     Counters.Counter private _knightCounter;
     Counters.Counter private _bishopCounter;
 
+    // Mapping to store the balance of each Piece type
+    mapping(Piece => uint256) public pieceBalance;
+
     constructor() ERC721("NumberRunnerClub", "NRC") {}
 
     function mint(Piece _piece, string memory tokenURI)
         public
+        payable
         returns (uint256)
     {
+        require(msg.value > 200000000000000000);
         uint256 newItemId = _tokenIds.current();
         require(newItemId < MAX_NFT_SUPPLY, "Maximum NFTs reached.");
         collection.push(_piece);
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
+
+        // Add the transaction fee to the piece's balance
+        pieceBalance[_piece] += 100000000000000000;
         _tokenIds.increment();
         return newItemId;
     }
