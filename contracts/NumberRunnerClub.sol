47b31c11041f80a5864309e6e94604e49e2e465a
dassman <lukas-8@live.fr>
2023-05-11 11:01:16 +0200

    add piece Details structure and split fee accross all minted type of tokens

--- contracts/NumberRunnerClub.sol
+++ contracts/NumberRunnerClub.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -14,25 +15,30 @@ contract NumberRunnerClub is ERC721URIStorage {
         Pawn
     }
 
+    struct PieceDetails {
+        uint256 maxSupply;
+        uint256 currentSupply;
+        uint256 percentage;
+    }
+
     Piece[] public collection;
     uint256 public constant MAX_NFT_SUPPLY = 10000;
-    uint256 public constant MAX_KING_SUPPLY = 2;
-    uint256 public constant MAX_QUEEN_SUPPLY = 10;
-    uint256 public constant MAX_ROOK_SUPPLY = 50;
-    uint256 public constant MAX_KNIGHT_SUPPLY = 100;
-    uint256 public constant MAX_BISHOP_SUPPLY = 200;
 
     Counters.Counter private _tokenIds;
-    Counters.Counter private _kingCounter;
-    Counters.Counter private _queenCounter;
-    Counters.Counter private _rookCounter;
-    Counters.Counter private _knightCounter;
-    Counters.Counter private _bishopCounter;
 
     // Mapping to store the balance of each Piece type
     mapping(Piece => uint256) public pieceBalance;
 
-    constructor() ERC721("NumberRunnerClub", "NRC") {}
+    mapping(Piece => PieceDetails) public pieceDetails;
+
+    constructor() ERC721("NumberRunnerClub", "NRC") {
+        pieceDetails[Piece.King] = PieceDetails(2, 0, 350);
+        pieceDetails[Piece.Queen] = PieceDetails(10, 0, 225);
+        pieceDetails[Piece.Rook] = PieceDetails(50, 0, 150);
+        pieceDetails[Piece.Knight] = PieceDetails(100, 0, 125);
+        pieceDetails[Piece.Bishop] = PieceDetails(200, 0, 100);
+        pieceDetails[Piece.Pawn] = PieceDetails(9638, 0, 650);
+    }
 
     function mint(Piece _piece, string memory tokenURI)
         public
@@ -42,12 +48,30 @@ contract NumberRunnerClub is ERC721URIStorage {
         require(msg.value > 200000000000000000);
         uint256 newItemId = _tokenIds.current();
         require(newItemId < MAX_NFT_SUPPLY, "Maximum NFTs reached.");
+        require(
+            pieceDetails[_piece].currentSupply < pieceDetails[_piece].maxSupply,
+            "Max supply for this piece type reached"
+        );
         collection.push(_piece);
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
+        pieceDetails[_piece].currentSupply++;
+
+        if (pieceDetails[_piece].currentSupply == 1) {
+            // If it's the first piece of this type
+            if(_piece != Piece.Pawn && _piece != Piece.King){
+                pieceDetails[Piece.Pawn].percentage -= pieceDetails[_piece].percentage;
+            }
+        }
 
         // Add the transaction fee to the piece's balance
-        pieceBalance[_piece] += 100000000000000000;
+        for (uint8 i = 0; i < 6; i++) {
+            PieceDetails memory piece = pieceDetails[Piece(i)];
+            if(piece.currentSupply > 0) {
+                uint256 pieceShare = (100000000000000 * piece.percentage);
+                pieceBalance[Piece(i)] += pieceShare;
+            }
+        }
         _tokenIds.increment();
         return newItemId;
     }
