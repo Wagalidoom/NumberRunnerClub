4d7e9d7a5152b1449d916ed83315cb82b2bff99c
dassman <lukas-8@live.fr>
2023-05-23 20:13:16 +0200

    update king auction for two kings

--- contracts/KingAuction.sol
+++ contracts/KingAuction.sol
@@ -4,54 +4,52 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract KingAuction {
-    address payable owner;
+    address owner;
     uint256 public auctionEndTime;
-    uint256 public highestBid;
+    uint256[2] public highestBid;
     uint256 public minPrice;
     uint256 public maxPrice;
-    address public highestBidder;
-
+    address[2] public highestBidder;
     ERC721 public nftContract;
-    uint256 public tokenId;
 
     event HighestBidIncreased(address bidder, uint256 amount);
     event AuctionEnded(address winner, uint256 amount);
 
     constructor(
         uint256 _biddingTime,
-        address _nftAddress,
-        uint256 _tokenId
+        address _nftAddress
     ) {
-        owner = payable(msg.sender);
+        owner = msg.sender;
         auctionEndTime = block.timestamp + _biddingTime;
         minPrice = 2 ether; // Minimum price at the end of auction
         maxPrice = 20000 ether; // Initial price at the start of auction
 
         nftContract = ERC721(_nftAddress);
-        tokenId = _tokenId;
     }
 
-    function bid() public payable {
+    function bid(uint256 _color) public payable {
         require(block.timestamp <= auctionEndTime, "Auction already ended.");
         uint256 currentPrice = getCurrentPrice();
         require(msg.value >= currentPrice, "The bid is too low.");
 
-        if (msg.value > highestBid) {
-            if (highestBidder != address(0)) {
+        if (msg.value > highestBid[_color]) {
+            if (highestBidder[_color] != address(0)) {
                 // Refund the old highest bidder
-                highestBidder.transfer(highestBid);
+                payable(highestBidder[_color]).transfer(highestBid[_color]);
             }
-            highestBidder = msg.sender;
-            highestBid = msg.value;
+            highestBidder[_color] = msg.sender;
+            highestBid[_color] = msg.value;
             emit HighestBidIncreased(msg.sender, msg.value);
         }
     }
 
     function auctionEnd() public {
         require(block.timestamp >= auctionEndTime, "The auction has not ended.");
-        emit AuctionEnded(highestBidder, highestBid);
-        nftContract.transferFrom(owner, highestBidder, tokenId);
-        owner.transfer(highestBid);
+        emit AuctionEnded(highestBidder[0], highestBid[0]);
+        emit AuctionEnded(highestBidder[1], highestBid[1]);
+        nftContract.transferFrom(owner, highestBidder[0], 0);
+        nftContract.transferFrom(owner, highestBidder[1], 1);
+        // owner.transfer(highestBid);
     }
 
     function getCurrentPrice() public view returns (uint256) {
